d6cbf717576eb061d86958613b6b09eab6faae36
Wan Zulmuhammad Harith <wzharith@gmail.com>
2020-09-19 15:30:54 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Bank Loan Application")
+st.subheader("A data mining project by Wan Zulmuhammad Harith, Amirah Anis Adlin and Nurlaili Hamimi")
+
+st.text("Deployment Phase")
+
+
